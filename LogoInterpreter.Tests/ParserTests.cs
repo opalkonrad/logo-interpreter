8805591a103e19a0cfd41aeef48a1d1af67b9d1a
Konrad Opalinski <opalkonrad@gmail.com>
2020-05-31 20:18:09 +0200

    Add some parser tests

---
+++ LogoInterpreter.Tests/ParserTests.cs
@@ -0,0 +1,155 @@
+﻿using LogoInterpreter.Interpreter;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.IO;
+
+namespace LogoInterpreter.Tests
+{
+    [TestClass]
+    public class ParserTests
+    {
+        private readonly StringWriter writer;
+
+        public ParserTests()
+        {
+            writer = new StringWriter();
+            Console.SetOut(writer);
+        }
+
+        [TestMethod]
+        [DataRow("a=x")]
+        [DataRow("b=(x)")]
+        [DataRow("c=-x")]
+        [DataRow("d=(-x)")]
+        [DataRow("e=1+2/3")]
+        [DataRow("f=foo(1)")]
+        public void ExecutorVisitorVisit_AssignmentStatement_CreateAssignmentStatementAST(string text)
+        {
+            var lexer = new Lexer(new StringSource(text));
+            var parser = new Parser(lexer);
+            var program = parser.Parse();
+            var testVisitor = new TestVisitor();
+            testVisitor.Visit(program);
+            Assert.AreEqual(text, writer.ToString());
+        }
+
+        [TestMethod]
+        [DataRow("a()")]
+        [DataRow("b(x)")]
+        [DataRow("c(-x)")]
+        [DataRow("d(x,y)")]
+        [DataRow("e(x,y,z)")]
+        [DataRow("f(10)")]
+        [DataRow("g(-10)")]
+        [DataRow("h(10,20)")]
+        [DataRow("i(10,-20)")]
+        [DataRow("j(10,x)")]
+        [DataRow("k(x,10)")]
+        [DataRow("l(\"lala\")")]
+        [DataRow("m(\"lala\",10)")]
+        [DataRow("n(\"lala\",10,x)")]
+        public void ExecutorVisitorVisit_FuncCall_CreateFuncCallAST(string text)
+        {
+            var lexer = new Lexer(new StringSource(text));
+            var parser = new Parser(lexer);
+            var program = parser.Parse();
+            var testVisitor = new TestVisitor();
+            testVisitor.Visit(program);
+            Assert.AreEqual(text, writer.ToString());
+        }
+
+        [TestMethod]
+        [DataRow("func a(){}", "func a(){}")]
+        [DataRow("func b(num x){}", "func b(NumToken x){}")]
+        [DataRow("func c(num x){num y}", "func c(NumToken x){NumToken y}")]
+        public void ExecutorVisitorVisit_FuncDefinition_CreateFuncDefinitionAST(string text, string expected)
+        {
+            var lexer = new Lexer(new StringSource(text));
+            var parser = new Parser(lexer);
+            var program = parser.Parse();
+            var testVisitor = new TestVisitor();
+            testVisitor.Visit(program);
+            Assert.AreEqual(expected, writer.ToString());
+        }
+
+        [TestMethod]
+        [DataRow("if(1){}", "if(1){}")]
+        [DataRow("if(-1){}", "if(-1){}")]
+        [DataRow("if(1<0){}", "if(1<0){}")]
+        [DataRow("if(1<=0){}", "if(1<=0){}")]
+        [DataRow("if(1>0){}", "if(1>0){}")]
+        [DataRow("if(1>0){}", "if(1>0){}")]
+        [DataRow("if(1>=0){}", "if(1>=0){}")]
+        [DataRow("if(1){}else{}", "if(1){}")]
+        [DataRow("if(-1){}else{}", "if(-1){}")]
+        [DataRow("if(1){num x}else{}", "if(1){NumToken x}")]
+        [DataRow("if(1){}else{num x}", "if(1){}else{NumToken x}")]
+        public void ExecutorVisitorVisit_IfStatement_CreateIfStatementAST(string text, string expected)
+        {
+            var lexer = new Lexer(new StringSource(text));
+            var parser = new Parser(lexer);
+            var program = parser.Parse();
+            var testVisitor = new TestVisitor();
+            testVisitor.Visit(program);
+            Assert.AreEqual(expected, writer.ToString());
+        }
+
+        [TestMethod]
+        [DataRow("a.Forward(100)")]
+        [DataRow("b.Right(90)")]
+        [DataRow("c.PenUp()")]
+        public void ExecutorVisitorVisit_MethCall_CreateMethCallAST(string text)
+        {
+            var lexer = new Lexer(new StringSource(text));
+            var parser = new Parser(lexer);
+            var program = parser.Parse();
+            var testVisitor = new TestVisitor();
+            testVisitor.Visit(program);
+            Assert.AreEqual(text, writer.ToString());
+        }
+
+        [TestMethod]
+        [DataRow("repeat(1){}")]
+        [DataRow("repeat(-1){}")]
+        public void ExecutorVisitorVisit_RepeatStatement_CreateRepeatStatementAST(string text)
+        {
+            var lexer = new Lexer(new StringSource(text));
+            var parser = new Parser(lexer);
+            var program = parser.Parse();
+            var testVisitor = new TestVisitor();
+            testVisitor.Visit(program);
+            Assert.AreEqual(text, writer.ToString());
+        }
+
+        [TestMethod]
+        [DataRow("return 1")]
+        [DataRow("return -1")]
+        [DataRow("return x")]
+        [DataRow("return -x")]
+        [DataRow("return \"lala\"")]
+        [DataRow("return 5-1")]
+        public void ExecutorVisitorVisit_ReturnStatement_CreateReturnStatementAST(string text)
+        {
+            var lexer = new Lexer(new StringSource(text));
+            var parser = new Parser(lexer);
+            var program = parser.Parse();
+            var testVisitor = new TestVisitor();
+            testVisitor.Visit(program);
+            Assert.AreEqual(text, writer.ToString());
+        }
+
+        [TestMethod]
+        [DataRow("str a", "StrToken a")]
+        [DataRow("num b", "NumToken b")]
+        [DataRow("Turtle c", "TurtleToken c")]
+        public void ExecutorVisitorVisit_VarDeclaration_CreateVarDeclarationAST(string text, string expected)
+        {
+            var lexer = new Lexer(new StringSource(text));
+            var parser = new Parser(lexer);
+            var program = parser.Parse();
+            var testVisitor = new TestVisitor();
+            testVisitor.Visit(program);
+            Assert.AreEqual(expected, writer.ToString());
+        }
+    }
+}
---
+++ LogoInterpreter/Interpreter/Executor/TestVisitor.cs
@@ -0,0 +1,253 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Documents;
+
+namespace LogoInterpreter.Interpreter
+{
+    public class TestVisitor : IVisitor
+    {
+        public void Visit(Program program)
+        {
+            foreach (INode stmt in program.Statements)
+            {
+                stmt.Accept(this);
+            }
+
+            foreach (KeyValuePair<string, FuncDefinition> funcDef in program.FuncDefinitions)
+            {
+                funcDef.Value.Accept(this);
+            }
+        }
+
+        public void Visit(FuncDefinition funcDef)
+        {
+            Console.Write("func ");
+            Console.Write(funcDef.Name);
+
+            Console.Write("(");
+            int argsCntr = funcDef.Parameters.Count;
+            foreach (VarDeclaration arg in funcDef.Parameters)
+            {
+                arg.Accept(this);
+
+                if (argsCntr != 1)
+                {
+                    Console.Write(",");
+                    argsCntr--;
+                }
+            }
+            Console.Write(")");
+
+            funcDef.Body.Accept(this);
+        }
+
+        public void Visit(AddExpression addExpr)
+        {
+            int operandCntr = 1;
+            addExpr.Operands[0].Accept(this);
+
+            foreach (string oper in addExpr.Operators)
+            {
+                switch (oper)
+                {
+                    case "+":
+                        Console.Write("+");
+                        break;
+
+                    case "-":
+                        Console.Write("-");
+                        break;
+                }
+
+                addExpr.Operands[operandCntr++].Accept(this);
+            }
+        }
+
+        public void Visit(AssignmentStatement assignStmt)
+        {
+            Console.Write($"{assignStmt.Variable}=");
+            assignStmt.RightSideExpression.Accept(this);
+        }
+
+        public void Visit(BlockStatement blockStmt)
+        {
+            Console.Write("{");
+            foreach (INode stmt in blockStmt.Statements)
+            {
+                stmt.Accept(this);
+            }
+            Console.Write("}");
+        }
+
+        public void Visit(EqualCondition equalCond)
+        {
+            int operandCntr = 1;
+            equalCond.Operands[0].Accept(this);
+
+            foreach (string oper in equalCond.Operators)
+            {
+                switch (oper)
+                {
+                    case "==":
+                        Console.Write("==");
+                        break;
+
+                    case "!=":
+                        Console.Write("!=");
+                        break;
+                }
+
+                equalCond.Operands[operandCntr++].Accept(this);
+            }
+        }
+
+        public void Visit(ExpressionExprParam exprExprParam)
+        {
+            Console.Write("(");
+            exprExprParam.Expression.Accept(this);
+            Console.Write(")");
+        }
+
+        public void Visit(FuncCall funcCall)
+        {
+            Console.Write(funcCall.Name);
+
+            Console.Write("(");
+            int argsCntr = funcCall.Arguments.Count;
+            foreach (AddExpression arg in funcCall.Arguments)
+            {
+                arg.Accept(this);
+
+                if (argsCntr != 1)
+                {
+                    Console.Write(",");
+                    argsCntr--;
+                }
+            }
+            Console.Write(")");
+        }
+
+        public void Visit(FuncCallExprParam funcCallExprParam)
+        {
+            Console.Write(funcCallExprParam.Unary ? "-" : "");
+            funcCallExprParam.FuncCall.Accept(this);
+        }
+
+        public void Visit(IdentifierExprParam identExprParam)
+        {
+            Console.Write(identExprParam.Unary ? $"-{identExprParam.Value}" : identExprParam.Value);
+        }
+
+        public void Visit(IfStatement ifStmt)
+        {
+            Console.Write("if");
+
+            Console.Write("(");
+            ifStmt.Condition.Accept(this);
+            Console.Write(")");
+
+            ifStmt.Body.Accept(this);
+
+            if (ifStmt.ElseBody.Statements.Count != 0)
+            {
+                Console.Write("else");
+
+                ifStmt.ElseBody.Accept(this);
+            }
+        }
+
+        public void Visit(MethCall methCall)
+        {
+            Console.Write($"{methCall.TurtleName}.{methCall.MethName}");
+
+            Console.Write("(");
+            methCall.Argument?.Accept(this);
+            Console.Write(")");
+        }
+
+        public void Visit(MultExpression multExpr)
+        {
+            int operandCntr = 1;
+            multExpr.Operands[0].Accept(this);
+
+            foreach (string oper in multExpr.Operators)
+            {
+                switch (oper)
+                {
+                    case "*":
+                        Console.Write("*");
+                        break;
+
+                    case "/":
+                        Console.Write("/");
+                        break;
+                }
+
+                multExpr.Operands[operandCntr++].Accept(this);
+            }
+        }
+
+        public void Visit(NumValueExprParam numValExprParam)
+        {
+            Console.Write(numValExprParam.Unary ? $"-{numValExprParam.Value}" : numValExprParam.Value.ToString());
+        }
+
+        public void Visit(RelationalCondition relCond)
+        {
+            int operandCntr = 1;
+            relCond.Operands[0].Accept(this);
+
+            foreach (string oper in relCond.Operators)
+            {
+                switch (oper)
+                {
+                    case "<":
+                        Console.Write("<");
+                        break;
+
+                    case "<=":
+                        Console.Write("<=");
+                        break;
+
+                    case ">":
+                        Console.Write(">");
+                        break;
+
+                    case ">=":
+                        Console.Write(">=");
+                        break;
+                }
+
+                relCond.Operands[operandCntr++].Accept(this);
+            }
+        }
+
+        public void Visit(RepeatStatement repeatStmt)
+        {
+            Console.Write("repeat");
+
+            Console.Write("(");
+            repeatStmt.NumOfTimes.Accept(this);
+            Console.Write(")");
+
+            repeatStmt.Body.Accept(this);
+        }
+
+        public void Visit(ReturnStatement retStmt)
+        {
+            Console.Write("return ");
+            retStmt.Expression.Accept(this);
+        }
+
+        public void Visit(StrValueExprParam strValExprParam)
+        {
+            Console.Write($"\"{strValExprParam.Value}\"");
+        }
+
+        public void Visit(VarDeclaration varDecl)
+        {
+            Console.Write($"{varDecl.Type} {varDecl.Name}");
+        }
+    }
+}
